0f3a24f3146126963a8bbf37c84f280919f782da
manenazaryan2006 <nazaryanmane496@gmail.com>
2024-10-17 23:03:12 +0400

    Create ex9.cpp

---
+++ ex9.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+   int gumar,poxarjeq;
+   std::cout<<"nermucel gumary ev poxarjeqy";
+   std::cin>>gumar>>poxarjeq;
+   std::cout<<gumar*poxarjeq;
+    return 0;
+}
